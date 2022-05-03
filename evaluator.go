16b54e7bdf03749e149752bf47e6163edcefa866
John Shiver <john@johnshiver.org>
2022-05-03 02:37:47 -0400

    eval

--- evaluator.go
+++ evaluator.go
@@ -2,8 +2,22 @@ package monkey_interpreter
 
 func Eval(node Node) Object {
 	switch node := node.(type) {
+	// Statements
+	case *Program:
+		return evalStatements(node.Statements)
+	case *ExpressionStatement:
+		return Eval(node.Expression)
+		// Expressions
 	case *IntegerLiteral:
 		return &Integer{Value: node.Value}
 	}
 	return nil
+}
+
+func evalStatements(stmts []Statement) Object {
+	var result Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
 }
