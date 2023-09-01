884388694188986714b76fa3948edc5e56deb540
John Shiver <john@johnshiver.org>
2023-09-01 19:15:44 -0400

    add hash key

--- object.go
+++ object.go
@@ -3,6 +3,7 @@ package monkey_interpreter
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 )
 
@@ -116,4 +117,27 @@ func (ao *Array) Inspect() string {
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
 	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *BooleanObject) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	_, _ = h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
 }
