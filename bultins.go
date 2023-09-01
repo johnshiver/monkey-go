a0872463d37ca60fb363d6906208fd6c44839d42
John Shiver <john@johnshiver.org>
2023-09-01 13:25:18 -0400

    add push built in

--- bultins.go
+++ bultins.go
@@ -1,7 +1,7 @@
 package monkey_interpreter
 
 var builtins = map[string]*Builtin{
-	"len": &Builtin{
+	"len": {
 		Fn: func(args ...Object) Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -9,9 +9,34 @@ var builtins = map[string]*Builtin{
 			switch arg := args[0].(type) {
 			case *String:
 				return &Integer{Value: int64(len(arg.Value))}
+			case *Array:
+				return &Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"push": {
+		Fn: func(args ...Object) Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			if args[0].Type() != ARRY_OBJ_TYPE {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*Array)
+
+			// NOTE: i would prefer to reuse the original array
+			// the book makes a new copy
+
+			//length := len(arr.Elements)
+			//newElements := make([]Object, length+1, length+1)
+			//copy(newElements, arr.Elements)
+			//newElements[length] = args[1]
+			arr.Elements = append(arr.Elements, args[1])
+			//return &Array{Elements: newElements}
+			return arr
 		},
 	},
 }
